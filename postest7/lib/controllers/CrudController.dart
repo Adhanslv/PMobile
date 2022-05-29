c31d940abcfc5289202931f41820760690143af1
Adhanslv <asyhab2002@gmail.com>
2022-05-29 17:27:07 +0800

    Postest7

---
+++ postest7/lib/controllers/CrudController.dart
@@ -0,0 +1,10 @@
+import 'package:get/get.dart';
+
+// Sebenarnya ini cuman dipakai pas update aja sih
+class CrudController extends GetxController {
+  var id = ''.obs;
+  var nama = ''.obs;
+  var merk = ''.obs;
+  var size = ''.obs;
+  var deskripsi = ''.obs;
+}
